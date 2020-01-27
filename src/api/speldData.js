d0327d8882f939f185d9ffd3a531cd3df70c6c31
Lilian <lilian@touchtribe.nl>
2020-01-27 16:20:46 +0100

    first commit

---
+++ src/api/speldData.js
@@ -0,0 +1,221 @@
+const speldData = [
+    {
+      id: 1,
+      name: 'wegslepen',
+      date: '',
+      author: '',
+      img: '',
+      title: '"Pak vaker de fiets!", zegt gemeente die elke dag een voetbalveld aan foutgeparkeerde fietsen laat verdwijnen',
+      subtitle: '',
+      text: "Mensen zouden vaker de auto moeten laten staan en op de fiets naar hun werk of het station gaan. Dat vinden gemeenten die dagelijks vrachtwagens vol fietsen laten afvoeren omdat ze een paar centimeter naast een vol rek zijn geparkeerd.\n" +
+        "\n" +
+        "“Fietsen is veel beter voor het klimaat”, zegt een woordvoerder van de gemeente Rotterdam, waar rond het Centraal Station iedere dag een voetbalveld aan foutgeparkeerde fietsen verdwijnt. Ook Amsterdam moedigt inwoners aan om vaker te kiezen voor de fiets, die wordt losgeknipt en afgevoerd als hij ook maar eventjes buiten een rek of vak staat.\n" +
+        "\n" +
+        "\n" +
+        "De gemeente Amersfoort gaat nog een stapje verder. Daar worden inwoners niet alleen aangespoord om zich fietsend naar het station te begeven op weg naar hun werk, maar ook om in hun vrije tijd lopend naar het fietsdepot te gaan in de hoop hun verwijderde fiets terug te vinden. “Zo dragen we allemaal een steentje bij aan de strijd tegen klimaatverandering.””,
+  },
+
+  {
+    id: 2,
+    name: 'pannenkoeken',
+    date: '',
+    author: '',
+    img: '',
+    title: "Bij deze pannenkoekenmix hoef je alleen maar alle ingrediënten voor pannenkoeken toe te voegen",
+    subtitle: "",
+    text: ""
+  },
+
+  {
+    id: 3,
+    name: "vaderdochter",
+    date: '',
+    author: '',
+    img: '',
+    title: 'Millennial-vader leest dochter sprookje voor over mensen met koophuizen en vaste contracten',
+    subtitle: '',
+    text: ''
+  },
+
+  {
+    id: 4,
+    name: 'contractgestoord',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Zzp\'er Reinier is een beetje contractgestoord',
+    subtitle: 'Het is ontzettend moeilijk om een overeenkomst met hem af te sluiten',
+    text: 'Voor zijn zakelijke omgeving komt hij wat vreemd over. Reinier Overduin (31) heeft een aandoening waardoor hij een beetje contractgestoord is. Reinier is zzp’er.\n' +
+      '“Het is ontzettend moeilijk om een contract met hem te krijgen”, vertelt Hans, teamleider bij het bedrijf waar Reinier weleens een dagje aan het koffie drinken is. “Het lijkt wel alsof hij overeenkomsten afstoot, alsof hij daar bang voor is of iets dergelijks. Maar wij weten hoe we daarmee om moeten gaan. We moeten vooral niet doen alsof hij zielig is, omdat hij zzp’er is.”\n' +
+      'Door zijn beperking komt Reinier maar weinig in aanraking met sociale zekerheid. Hij krijgt hiervoor hulp, zodat hij zich kan redden in de maatschappij, maar helemaal normaal zal hij vermoedelijk nooit worden.',
+  },
+
+  {
+    id: 5,
+    name: 'awareness',
+    date: '',
+    author: '',
+    img: '',
+    title: 'aware',
+    subtitle: '',
+    text: 'Er lijkt voor bijna elk onderwerp om meer awareness gevraagd te worden met acties en evenementen. Maar hebben we wel voldoende aandacht voor awareness? Experts pleiten voor meer bewustwording rondom awareness.\n' +
+      '\n' +
+      'Suzanne Heiting, hoogleraar psychologie aan de Universiteit van Harderwijk, wil meer belangstelling voor aandacht. “Veel mensen staan er onvoldoende bij stil dat ze bewust moeten zijn van het gebrek aan aandacht voor awareness. Er is meer oplettendheid voor nodig, anders is niemand straks meer aware.”\n' +
+      '\n' +
+      '\n' +
+      'Awareness dreigt in de vergetelheid te geraken, we durven er niet meer over te praten. Dat zegt mindfulness-coach Sjaak Debrei: “We moeten awareness uit het verdomhoekje halen. Het taboe rond awareness kan doorbroken worden. Awareness moet dan wel weer op de kaart gezet worden. Dan moet je denken aan een actie.”'
+  },
+
+  {
+    id: 6,
+    name: 'telefoonbetalen',
+    date: '',
+    author: '',
+    img: '',
+    title: 'ING financiert corrupt project Angola, maar je kan wel heel chill met je telefoon betalen',
+    subtitle: '',
+    text: 'Uit een onderzoek van het FD en Trouw blijkt dat de ING Bank 400 miljoen dollar heeft geleend aan de Angolese staat voor een project waarbij drieduizend families uit hun woningen zijn verjaagd. Niet goed, maar wat wel chill is, is dat je bij de ING gewoon met je telefoon kunt betalen zonder vervelend apparaatje. Je hoeft alleen maar een zelfgekozen code in te vullen.\n' +
+      '\n' +
+      'Volgens voormalige bewoners die door het leger en de politie uit hun huis werden gejaagd om plaats te maken voor een luxe boulevard zijn er tijdens de ontruiming twee kinderen doodgereden. Bovendien ontving niemand financiële compensatie voor de gesloopte woningen. Maar: bij ING kun je wel met Apple Pay contactloos betalen, daar heb je niet eens meer een pinpas voor nodig. Handig!'
+  },
+
+  {
+    id: 7,
+    name: 'kroeg-dj',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Ongelofelijk multitalent werkt niet alleen in een bar maar is ook dj',
+    subtitle: '',
+    text: 'Zo, nu hebben we er toch een gevonden: een jongen die niet alleen in een bar werkt, maar die ook nog eens dj is! Dit multitalent kan het allemaal en weet het ook nog eens te combineren: in een bar werken, muziek draaien. Heeft een dag voor hem dan meer dan 24 uur? \n' +
+      '\n' +
+      '“Vanavond moet ik nog draaien”, zegt de man. We zijn verbaasd, hij werkt toch in een bar? “Ja dat klopt, maar ik ben ook dj!” Damn…'
+  },
+
+  {
+    id: 8,
+    name: 'telefoon-kijken',
+    date: '',
+    author: '',
+    img: '',
+    title: 'De 89e keer dat Yara deze ochtend op haar telefoon kijkt is het precies 11:11',
+    subtitle: '',
+    text: 'Een bijzondere gebeurtenis voor de 27-jarige Yara. Toen ze deze ochtend voor de 89e keer op haar telefoon keek, was het precies 11.11 uur. Dit is een teken, weet ze.\n' +
+      '\n' +
+      'Yara maakt meteen een screenshot van haar scherm en appt haar beste vriendin. “Haha, alwéér! Zo bijzonder.” Haar vriendin bevestigt dat dit een teken is. “Nou hoe duidelijk wil je het hebben Yaar? Go for it.”\n' +
+      '\n' +
+      '\n' +
+      'Ook de laatste sporen van twijfel worden weggenomen als Yara de rest van de dag op alle bijzondere tijdstippen als 12:12, 13:13, 22:22 en alles ertussenin op haar telefoon kijkt. De boodschap is glashelder. Ze pakt haar telefoon en belt haar ex.'
+  },
+
+  {
+    id: 9,
+    name: 'notitieblokje',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Inspirerende notitieboekjes waarin je alleen op de eerste drie pagina\'s gaat schrijven',
+    subtitle: 'Notitieboekjes: je hebt er nooit genoeg. Zodra het enthousiasme waarmee je de eerste bladzijden vol schreef wegvaagt, kun je datzelfde boekje natuurlijk niet meer gebruiken. Een nieuwe is dus een must. Wij hebben de leukste nieuwe lege boekjes voor je op een rij gezet.',
+    text: 'Notitieboekjes: je hebt er nooit genoeg. Zodra het enthousiasme waarmee je de eerste bladzijden vol schreef wegvaagt, kun je datzelfde boekje natuurlijk niet meer gebruiken. Een nieuwe is dus een must. Wij hebben de leukste nieuwe lege boekjes voor je op een rij gezet.\n' +
+      '\n' +
+      'Moleskine classic zwart\n' +
+      'De klassieke zwarte Moleskine is chic en zakelijk. Perfect om de to-do-lijst daarin, die je nu al weken doorschuift naar morgen, met boek en al weg te gooien.\n' +
+      '\n' +
+      '\n' +
+      'Journaling-dagboekje\n' +
+      'Dit is een goede optie als je elke ochtend al je gedachten op wil gaan schrijven. Goed voor de creativiteit. Jammer dat je na drie dagen beseft dat creativiteit leuk is, maar een half uur langer slapen ook.\n' +
+      '\n' +
+      'Authentiek boekje van stof, met veters, van dat ene leuke marktje op vakantie\n' +
+      'Het boekje dat je kocht op die markt in India waar je al je reisbelevenissen in wilde schrijven. Dat was heel even inspirerend. Jammer dat je gewoon op vakantie was en twee dagen later weer op kantoor moest zijn.\n' +
+      '\n' +
+      'Daily greatness planner\n' +
+      'Deze planner helpt om je droomleven te leven. Bizar genoeg blijkt dat droomleven toch niet te bestaan uit constant doelen formuleren en in boekjes schrijven. \n' +
+      '\n' +
+      'Schattig klein boekje met geborduurde bloemetjes\n' +
+      'Prima om een keer een lijst met extreem gezonde boodschappen in te krassen en daarna te gebruiken om stukjes papier uit te scheuren als je bent uitgekauwd op je kauwgom.'
+  },
+
+  {
+    id: 10,
+    name: 'baudet',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Baudet: "Canada haalde toestel neer"',
+    subtitle: 'Niet Iran, maar Canada heeft het Oekraiense toestel bij Teheran uit de lucht geschoten.',
+    text: 'Niet Iran, maar Canada heeft het Oekraïense toestel bij Teheran uit de lucht geschoten. Die conclusie trekt Forum voor Democratie-leider Thierry Baudet na het analyseren van zijn eigen hersenkronkels. Baudet heeft een brief aan de Oekraïense president Zelensky ondertekend met het verzoek deze mogelijkheid te onderzoeken.'
+  },
+
+  {
+    id: 11,
+    name: 'museumjaarkaart',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Met de museumkaart kun je een jaar lang onbeperkt zeggen dat je een museumkaart hebt',
+    subtitle: '',
+    text: 'Vind jij het vervelend om in een museum een kaartje te moeten kopen? Vind je het leuk om tegen kennissen te zeggen dat je een museumkaart bezit? Dan is de museumkaart iets voor jou. Voor maar 65 euro kun jij een jaar lang onbeperkt zeggen dat je een museumkaart hebt.\n' +
+      '\n' +
+      'Leuke bijkomstigheid van de kaart is dat je ook nog eens naar verschillende musea kan, maar dat hoeft natuurlijk niet. Je moet vooral doen waar je zelf zin in hebt: tegen je vrienden zeggen dat je in het bezit bent van een kaart. Welke kaart? De museumkaart.'
+  },
+
+  {
+    id: 12,
+    name: 'maandag-kantoor',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Tom Dumoulin gaat elke maandagochtend eerst een uurtje naar kantoor',
+    subtitle: '"je voelt je de rest van de week beter"',
+    text: 'Het is vaste prik in huize Dumoulin: elke maandagochtend om stipt zeven uur gaat de beroepsrenner niet naar zijn werk, maar gaat hij eerst een uur naar kantoor. Om zijn hoofd leeg te maken, en daarna fris te zijn voor een nieuwe week. Geen maandag slaat-ie over.\n' +
+      '\n' +
+      '“Je zit niet meteen in die hectiek van zo’n werkweek als je eerst een uurtje glazig naar excelsheets hebt zitten kijken. Of de targets hebt besproken voor aankomende week”, zegt Dumoulin. Vrienden verklaren hem voor gek. “Ja, die snappen oprecht niet dat ik dit elke maandagochtend kan opbrengen. ‘Elke maandagochtend naar kantoor?’, lachen ze dan. Maar ja, als je de Tour wil winnen, moet je soms ook je zinnen kunnen verzetten”, zegt Dumoulin. “Echt hoor: je voelt je de rest van de week beter. En daarbij: op m’n reet zitten kan de hele week nog.”'
+  },
+
+  {
+    id: 13,
+    name: 'tussenjaar',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Komt door klimaatverandering het tussenjaar in gevaar?',
+    subtitle: '',
+    text: 'Bosbranden in Australië en Zuid-Amerika. Overstromingen in Indonesië. Wateroorlogen in Thailand. Extreme droogte in Zuid-Afrika. Het roept de prangende vraag op: komt door klimaatverandering ons tussenjaar in gevaar?\n' +
+      '\n' +
+      '“Het was verschrikkelijk”, vertelt de 19-jarige Joanne over haar ervaring met het boeken van een reis. “Ik keek naar de KLM Werelddeal Weken, maar kon geen kant op. Links waren overstromingen, rechts bosbranden. Ik kon nog net uitwijken naar Argentinië, maar ik heb vrienden die het niet redden en noodgedwongen achter moesten blijven in Nederland. Daar kan ik ‘s nachts niet van slapen, maar dat zou ook de jetlag kunnen zijn.”\n' +
+      '\n' +
+      '\n' +
+      'Ook de 20-jarige Alex zag met eigen ogen hoe het tussenjaar werd aangetast. Hij maakte een trip langs de 16 beste surfspots ter wereld. “Bali, Californië, Zuid-west Australië, Fiji. Maar holyshit dude, de waves zijn daar echt niet meer wat ze in de jaren ‘70 waren. Op sommige plekken tot wel 18 procent lager. Dagenlang van die klotsbakken.”  \n' +
+      '\n' +
+      'De zus van de 18-jarige Tessa gaf twee jaar geleden een paar maanden Engelse les op een school in Indonesië, maar toen Tessa zich ook wilde opgeven bleek ze te laat. “Superleuk, met van die schattige kindjes. Maar nu is blijkbaar dat hele dorp van de kaart geveegd. Het enige wat nog over is is vier maanden lang hulplokalen bouwen, maar dan ga ik liever gewoon extra lang skiën. Het is roeien met de riemen die je hebt.” '
+  },
+
+  {
+    id: 14,
+    name: 'boomplanten',
+    date: '',
+    author: '',
+    img: 'Hovenier Ludo maakt een vliegreis elke keer dat ie een boom plant',
+    title: '',
+    subtitle: '"ik plant volledig klimaatneutraal"',
+    text: 'De 41-jarige Ludo plant heel veel bomen, maar hij compenseert dat wel. Elke keer dat ie een boom de grond in stopt, boekt ie ook een vliegreis.\n' +
+      '\n' +
+      '“Eén boom is een reis naar Londen. Tien bomen is een reis naar Thailand. En als ik vijftig bomen plant, ga ik naar Australië”, vertelt Ludo. “Zo plant ik volledig klimaatneutraal. Sja, je doet wat je kunt.”'
+  },
+
+  {
+    id: 15,
+    name: 'klasgenoot',
+    date: '',
+    author: '',
+    img: '',
+    title: 'Klasgenoot van vroeger hoeft geen verjaardagscadeau, alleen jouw geld voor kinderkanker',
+    subtitle: '',
+    text: 'Ooit zaten jullie naast elkaar bij Engels. Inmiddels heb je Janine al vijftien jaar niet gezien. Maar trouwe Facebookvrienden zijn jullie wel. En nu strijdt Janine ineens tegen kinderkanker, van jouw geld.\n' +
+      '\n' +
+      'Janine schrijft op haar profiel: ‘Op mijn vorige verjaardag kreeg ik een prachtige sporttas. Sindsdien heb ik alles wat mijn hartje begeert. Ik zou niet weten wat er nog bij zou kunnen. Behalve dan leven in een wereld zonder kinderkanker. Daarom pak ik het dit jaar anders aan en vraag ik geld voor het goede doel. Aan iedereen. Want wat is nou een mooier cadeau aan jezelf dan aandacht vragen voor iets dat groter is dan jezelf? Met één klik op de knop kunnen al mijn vrienden laten zien dat ze kinderkanker net zo belangrijk vinden als mijn verjaardag.’\n' +
+      '\n' +
+      '\n' +
+      'En zo zit jij ineens met een dilemma: wil je ook een wereld zonder kinderkanker of een wereld zonder Janine als Facebook-vriend? De keuze is aan jou.'
+  }
+];
---
+++ src/api/users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    id: 1,
+    role: 'admin',
+    name: 'Lilian'
+  },
+  {
+    id: 2,
+    role: 'user',
+    name: 'Lisanne'
+  }
+];
